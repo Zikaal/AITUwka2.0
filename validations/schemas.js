3445c6f9953a5bfa4c139af023d886e7e2704b89
Zikaal <168971723+Zikaal@users.noreply.github.com>
2025-02-24 22:49:15 +0500

    Update schemas.js

--- validations/schemas.js
+++ validations/schemas.js
@@ -15,7 +15,7 @@ const registerSchema = Joi.object({
   password: Joi.string()
     .min(6)
     .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).+$/)
-    .message('Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one digit, and one special character.')
+    .message('Password must be at least 6 characters long and include at least one uppercase letter, one lowercase letter, one digit, and one special character.')
     .required()
 });
 
