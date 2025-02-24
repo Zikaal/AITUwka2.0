c1c9c1690e5501e911544fec8a53a27d3a6f71ad
Zikaal <168971723+Zikaal@users.noreply.github.com>
2025-02-24 22:48:37 +0500

    Add files via upload

--- validations/schemas.js
+++ validations/schemas.js
@@ -1,14 +1,29 @@
 const Joi = require('joi');
 
 const registerSchema = Joi.object({
-  username: Joi.string().min(3).max(30).required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required()
+  username: Joi.string()
+    .min(3)
+    .max(16)
+    .pattern(/^[A-Za-z]+$/)
+    .message('Username must contain only letters (A-Z, a-z) and be between 3 and 16 characters long.')
+    .required(),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .pattern(/^[\w.+-]+@[\w-]+\.[\w.-]+$/)
+    .message('Email format is invalid.')
+    .required(),
+  password: Joi.string()
+    .min(6)
+    .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).+$/)
+    .message('Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one digit, and one special character.')
+    .required()
 });
 
 const loginSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required()
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required(),
+  password: Joi.string().min(6).required() 
 });
 
 const postSchema = Joi.object({
